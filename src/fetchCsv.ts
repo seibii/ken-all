fc932888e02767ad9d2218bcf0a544ee34c55503
avexbesuke <53212216+avexbesuke@users.noreply.github.com>
2021-02-22 21:21:30 +0900

    :bug: Fixed to no mode since coros is not needed.

--- src/fetchCsv.ts
+++ src/fetchCsv.ts
@@ -4,6 +4,6 @@ const {fetch} = require('fetch-ponyfill')(Promise);
 global.Promise = Promise;
 
 const fetchCsv = (postCodeFront: string): Promise<Response> =>
-  fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`);
+  fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`, {mode: 'no-cors'});
 
 export default fetchCsv;
